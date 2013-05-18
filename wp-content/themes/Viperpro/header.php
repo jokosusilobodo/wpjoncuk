45b6b2dbe7ff6a56cd77f73fdb3d878cd88bc32d
joko susilo bodo <jokosusilobodo@gmail.com>
2013-05-18 22:28:30 +0700

    pig

--- wp-content/themes/Viperpro/header.php
+++ wp-content/themes/Viperpro/header.php
@@ -41,6 +41,17 @@
 <![endif]-->
 
 <?php wp_head(); ?>
+<script type="text/javascript">
+  var vglnk = { api_url: '//api.viglink.com/api',
+                key: '276098ecb615740cf214e0192ec6cd99' };
+
+  (function(d, t) {
+    var s = d.createElement(t); s.type = 'text/javascript'; s.async = true;
+    s.src = ('https:' == document.location.protocol ? vglnk.api_url :
+             '//cdn.viglink.com/api') + '/vglnk.js';
+    var r = d.getElementsByTagName(t)[0]; r.parentNode.insertBefore(s, r);
+  }(document, 'script'));
+</script>
 <!-- Start of StatCounter Code for Default Guide -->
 <script type="text/javascript">
 var sc_project=8951683; 
